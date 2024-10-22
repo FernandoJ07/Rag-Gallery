1fb58d3fe8acc8826095941adf4fcc3d414b492f
FernandoJ07 <fernandogerdez@gmail.com>
2024-10-22 17:26:24 -0500

    Cors para aceptar solicitudes del fronted

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 # app/main.py
 from fastapi import FastAPI
 from app.api import routers
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(routers.rag_router)
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 if __name__ == "__main__":
     import uvicorn
